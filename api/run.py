f4c6204b76909557804b810738db810ed825f0d4
Muhammad Abyan Rizqo <muhammadabyan@refactory.id>
2022-02-24 08:58:04 +0700

    setup port

--- api/run.py
+++ api/run.py
@@ -1,3 +1,10 @@
 from __init__ import app
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Get config
+PORT = getenv('PORT', '80')
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=PORT)
